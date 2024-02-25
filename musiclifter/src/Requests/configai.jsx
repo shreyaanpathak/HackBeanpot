6b9f14b7c2ce8e95c83b8ec1a6daf3b6fbb9c5f5
shreyaanpathak <33208813+shreyaanpathak@users.noreply.github.com>
2024-02-25 08:51:56 -0500

    Update configai.jsx

--- musiclifter/src/Requests/configai.jsx
+++ musiclifter/src/Requests/configai.jsx
@@ -1,6 +1,6 @@
 import OpenAI from 'openai';
 
 export const openai = new OpenAI({
-    apiKey : "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", dangerouslyAllowBrowser: true
+    apiKey : "ENTER YOUR API KEY HERE", dangerouslyAllowBrowser: true
   })
   
